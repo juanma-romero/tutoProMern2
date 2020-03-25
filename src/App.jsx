de6e7aae5d946d541babf229b4f795b2a2accd89
juanma-romero <xjuanma.romerox@gmail.com>
2020-03-25 00:12:20 -0300

    ok hasta pag 78, event handling

---
+++ src/App.jsx
@@ -0,0 +1,147 @@
+const initialIssues = [
+    {
+        id: 1, status: 'New', owner: 'Ravan', effort: 5,
+        created: new Date('2018-08-15'), due: undefined,
+        title: 'Error in console when clicking Add',
+        },
+    {
+        id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+        created: new Date('2018-08-16'), due: new Date('2018-08-30'),
+        title: 'Missing bottom border on panel',
+    },
+    {
+        id: 3, status: 'New', owner: 'Juancho', effort: 24,
+        created: new Date('2020-03-16'), due: new Date('2020-03-28'),
+        title: 'pantallazo',
+    },
+]
+
+
+class IssueRow extends React.Component {
+    
+    render () {
+        const issue = this.props.issue
+        return (
+            <tr>
+                <td>{issue.id}</td>
+                <td>{issue.status}</td>
+                <td>{issue.owner}</td>
+                <td>{issue.created.toDateString()}</td>
+                <td>{issue.effort}</td>
+                <td>{issue.due ? issue.due.toDateString() : " "}</td>
+                <td>{issue.title}</td>
+            </tr>
+
+        )
+    }
+}
+
+class IssueFilter extends React.Component {
+    render () {
+        return (
+            <div>este lugar es para el filtro</div>
+        )
+    }
+}
+
+
+
+class IssueTable extends React.Component {
+    
+
+    render () {
+        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
+        
+        return (
+            <table className="bordered-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Status</th>
+                        <th>Owner</th>
+                        <th>Created</th>
+                        <th>Effort</th>
+                        <th>Due Date</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {issueRows} 
+                </tbody>
+            </table>
+        )
+    }
+}
+
+class IssueAdd extends React.Component {
+    constructor() {
+        super()
+        this.handleSubmit = this.handleSubmit.bind(this)
+        }
+
+    handleSubmit (e) {
+        e.preventDefault()
+        const form = document.forms.issueAdd
+        const issue = { owner: form.owner.value, title: form.title.value, status: "new"}
+        this.props.createIssue(issue)
+        
+        form.owner.value= ""
+        form.title.value= ""
+    }    
+
+    render () {
+        return (
+            <form name="issueAdd" onSubmit={this.handleSubmit}>
+                <input type="text" name="owner" placeholder="Owner" />
+                <input type="text" name="title" placeholder="Title" />
+                <button>Add</button>
+            </form>
+            
+        )
+    }
+}
+
+class IssueList extends React.Component {
+
+    constructor() {
+        super()
+        this.state = { issues: [] }
+        this.createIssue = this.createIssue.bind(this)
+        
+    }
+       
+    componentDidMount () {
+        this.loadData()
+    }
+
+    loadData() {
+        setTimeout( () => {this.setState({ issues: initialIssues})}, 500)
+         
+    }
+
+    createIssue(issue) {
+        issue.id = this.state.issues.length + 1;
+        issue.created = new Date();
+        const newIssueList = this.state.issues.slice();
+        newIssueList.push(issue);
+        this.setState({ issues: newIssueList });
+    }
+
+    render () {
+        return (
+            <React.Fragment>
+                <h1>Seguidor de trakers</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable issues={this.state.issues}/>
+                <hr />
+                <IssueAdd createIssue={this.createIssue} />
+            </React.Fragment>
+        )
+    }
+}
+
+
+const element = <IssueList />
+
+ReactDOM.render(element, document.getElementById('contents'));
